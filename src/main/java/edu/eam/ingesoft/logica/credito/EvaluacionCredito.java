9d6ddda9952b7769c952008e10c3878411be2fa9
stiben ruiz <ruiz.stiben.8154@eam.edu.co>
2025-09-19 18:11:04 -0500

    solucion trabajo 5

--- src/main/java/edu/eam/ingesoft/logica/credito/EvaluacionCredito.java
+++ src/main/java/edu/eam/ingesoft/logica/credito/EvaluacionCredito.java
@@ -7,63 +7,121 @@ public class EvaluacionCredito {
     private int numeroCreditosActivos;
     private int puntajeCredito;
     private double valorCreditoSolicitado;
-    private boolean tieneCodedor;
-    
-    public EvaluacionCredito(String nombreSolicitante, double ingresosMensuales, 
-                            int numeroCreditosActivos, int puntajeCredito, 
-                            double valorCreditoSolicitado, boolean tieneCodedor) {
+    private boolean tieneCodeudor;
+
+    // Constructor
+    public EvaluacionCredito(String nombreSolicitante, double ingresosMensuales,
+                             int numeroCreditosActivos, int puntajeCredito,
+                             double valorCreditoSolicitado, boolean tieneCodeudor) {
         this.nombreSolicitante = nombreSolicitante;
         this.ingresosMensuales = ingresosMensuales;
         this.numeroCreditosActivos = numeroCreditosActivos;
         this.puntajeCredito = puntajeCredito;
         this.valorCreditoSolicitado = valorCreditoSolicitado;
-        this.tieneCodedor = tieneCodedor;
+        this.tieneCodeudor = tieneCodeudor;
     }
-    
+
+    // Calcular tasa mensual (simplificado)
     public double calcularTasaMensual(double tasaNominalAnual) {
-        return (tasaNominalAnual / 100.0) / 12.0;
+        double tasa = tasaNominalAnual / 12;
+        return tasa;
     }
-     
+
+    // Calcular cuota mensual (simplificado con if/else)
     public double calcularCuotaMensual(double tasaNominalAnual, int plazoMeses) {
-        double im = calcularTasaMensual(tasaNominalAnual); 
-        if (im == 0) {
-            return valorCreditoSolicitado / plazoMeses; 
+        double tasaMensual = calcularTasaMensual(tasaNominalAnual);
+
+        if (tasaMensual == 0) {
+            return valorCreditoSolicitado / plazoMeses;
+        } else {
+            // Cálculo muy básico (sin fórmula completa)
+            double interes = valorCreditoSolicitado * (tasaMensual / 100);
+            double cuota = (valorCreditoSolicitado + interes) / plazoMeses;
+            return cuota;
         }
-        double factor = Math.pow(1 + im, plazoMeses);
-        double cuota = valorCreditoSolicitado * (im * factor) / (factor - 1);
-        return Math.round(cuota * 100.0) / 100.0; 
     }
-    
-    
+
+    // Evaluar aprobación con if/else básicos
     public boolean evaluarAprobacion(double tasaNominalAnual, int plazoMeses) {
         double cuota = calcularCuotaMensual(tasaNominalAnual, plazoMeses);
 
-        
         if (puntajeCredito < 500) {
-            return false;
+            return false; // perfil bajo
+        } else {
+            if (puntajeCredito >= 500 && puntajeCredito <= 700) {
+                if (tieneCodeudor) {
+                    if (cuota <= ingresosMensuales * 0.25) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            } else {
+                if (puntajeCredito > 700) {
+                    if (numeroCreditosActivos < 2) {
+                        if (cuota <= ingresosMensuales * 0.30) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            }
         }
-        
-        if (puntajeCredito >= 500 && puntajeCredito <= 700) {
-            return tieneCodedor && cuota <= ingresosMensuales * 0.25;
-        }
-        
-        if (puntajeCredito > 700 && numeroCreditosActivos < 2) {
-            return cuota <= ingresosMensuales * 0.30;
-        }
-
-        return false;
     }
-    
-    public String getNombreSolicitante() { return nombreSolicitante; }
-    public void setNombreSolicitante(String nombreSolicitante) { this.nombreSolicitante = nombreSolicitante; }
-    public double getIngresosMensuales() { return ingresosMensuales; }
-    public void setIngresosMensuales(double ingresosMensuales) { this.ingresosMensuales = ingresosMensuales; }
-    public int getNumeroCreditosActivos() { return numeroCreditosActivos; }
-    public void setNumeroCreditosActivos(int numeroCreditosActivos) { this.numeroCreditosActivos = numeroCreditosActivos; }
-    public int getPuntajeCredito() { return puntajeCredito; }
-    public void setPuntajeCredito(int puntajeCredito) { this.puntajeCredito = puntajeCredito; }
-    public double getValorCreditoSolicitado() { return valorCreditoSolicitado; }
-    public void setValorCreditoSolicitado(double valorCreditoSolicitado) { this.valorCreditoSolicitado = valorCreditoSolicitado; }
-    public boolean isTieneCodedor() { return tieneCodedor; }
-    public void setTieneCodedor(boolean tieneCodedor) { this.tieneCodedor = tieneCodedor; }
+
+    // Getters y setters
+    public String getNombreSolicitante() {
+        return nombreSolicitante;
+    }
+
+    public void setNombreSolicitante(String nombreSolicitante) {
+        this.nombreSolicitante = nombreSolicitante;
+    }
+
+    public double getIngresosMensuales() {
+        return ingresosMensuales;
+    }
+
+    public void setIngresosMensuales(double ingresosMensuales) {
+        this.ingresosMensuales = ingresosMensuales;
+    }
+
+    public int getNumeroCreditosActivos() {
+        return numeroCreditosActivos;
+    }
+
+    public void setNumeroCreditosActivos(int numeroCreditosActivos) {
+        this.numeroCreditosActivos = numeroCreditosActivos;
+    }
+
+    public int getPuntajeCredito() {
+        return puntajeCredito;
+    }
+
+    public void setPuntajeCredito(int puntajeCredito) {
+        this.puntajeCredito = puntajeCredito;
+    }
+
+    public double getValorCreditoSolicitado() {
+        return valorCreditoSolicitado;
+    }
+
+    public void setValorCreditoSolicitado(double valorCreditoSolicitado) {
+        this.valorCreditoSolicitado = valorCreditoSolicitado;
+    }
+
+    public boolean isTieneCodeudor() {
+        return tieneCodeudor;
+    }
+
+    public void setTieneCodeudor(boolean tieneCodeudor) {
+        this.tieneCodeudor = tieneCodeudor;
+    }
 }
